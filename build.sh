3590f5c7f760fa604cca3fcec04fc7d261bb7bf8
Sangeetha S <ssang@who.int>
2024-06-24 16:55:12 +0530

    feat: added the config and Dockerfile

---
+++ build.sh
@@ -0,0 +1,12 @@
+cd $(pwd)
+
+version='0.3'
+
+docker build -t whoalxp.azurecr.io/docker-clamav:openshift.$version .
+
+
+## uncomment the below lines when pushing to Quay
+
+# docker login quay.io
+
+# docker push whoalxp.azurecr.io/docker-clamav:openshift.$version
