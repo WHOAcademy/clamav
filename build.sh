7be6909e0f050906479e056f8b452348b5b3b63e
enparthasarathi <Parthasarathi.G@encora.com>
2025-03-25 13:42:54 +0530

    fix: update image version

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 cd $(pwd)
 
-version='0.3'
+version='base'
 
-docker build -t whoalxp.azurecr.io/docker-clamav:openshift-base.$version .
+docker build -t whoalxp.azurecr.io/docker-clamav:openshift-$version .
 
 
 ## uncomment the below lines when pushing to Quay
 
 # docker login quay.io
 
-# docker push whoalxp.azurecr.io/docker-clamav:openshift-base.$version
+# docker push whoalxp.azurecr.io/docker-clamav:openshift-$version
